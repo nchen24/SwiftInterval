10e647f5a1dce0a0d462120b79224a1f866e15b6
Nick Chen <nchen24@gmail.com>
2014-10-03 16:06:34 +0900

    Adds ..< for open interval.

--- openEnd.swift
+++ openEnd.swift
@@ -7,6 +7,17 @@ postfix operator ... {}
 postfix func ...(index: Int) -> OpenEnd {
     return OpenEnd(start: index)
 }
+
+// a[x..<]
+struct OpenEndN {
+    let start: Int
+}
+
+postfix operator ..< {}
+postfix func ..<(index: Int) -> OpenEndN {
+    return OpenEndN(start: index)
+}
+
 // a[...x]
 struct OpenStart {
     let end: Int
@@ -15,6 +26,12 @@ struct OpenStart {
 prefix operator ... {}
 prefix func ...(index: Int) -> OpenStart{
     return OpenStart(end: index)
+}
+
+// a[..<x]
+prefix operator ..< {}
+prefix func ..<(index: Int) -> OpenStart{
+    return OpenStart(end: index-1)
 }
 
 // a[x...y], a[-x...y], a[x...-y], a[-x...-y]
@@ -39,6 +56,9 @@ extension Array {
     subscript (oE: OpenEnd) -> Slice<Element> {
         return self[oE.start...self.count-1]
     }
+    subscript (oEN: OpenEndN) -> Slice<Element> {
+        return self[oEN.start...self.count-2]
+    }
     subscript (oS: OpenStart) -> Slice<Element> {
         return self[0...oS.end]
     }
@@ -59,15 +79,21 @@ let x = [0,1,2,3,4,5,6,7,8,9]
 let a = x[4...]
 let b = x[(-3)...]
 
-let c = x[...6]
-let d = x[...(-4)]
+let c = x[4..<]
+let d = x[(-3)..<]
 
-let e = x[2...5]
-let f = x[3...(-1)]
-let g = x[(-5)...8]
-let h = x[(-5)...(-2)]
+let e = x[...6]
+let f = x[...(-4)]
 
-let i = x[2..<5]
-let j = x[3..<(-1)]
-let k = x[(-5)..<8]
-let l = x[(-5)..<(-2)]
+let g = x[..<6]
+let h = x[..<(-4)]
+
+let i = x[2...5]
+let j = x[3...(-1)]
+let k = x[(-5)...8]
+let l = x[(-5)...(-2)]
+
+let m = x[2..<5]
+let n = x[3..<(-1)]
+let o = x[(-5)..<8]
+let p = x[(-5)..<(-2)]
