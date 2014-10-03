b529f351372d8f5215c070c11d34d58c04853239
Nick Chen <nchen24@gmail.com>
2014-10-03 16:51:35 +0900

    Adds step functionality

--- openEnd.swift
+++ openEnd.swift
@@ -56,44 +56,103 @@ extension Array {
     subscript (oE: OpenEnd) -> Slice<Element> {
         return self[oE.start...self.count-1]
     }
+
+    subscript (oE: OpenEnd, stride: Int) -> Slice<Element> {
+        return self[oE.start...self.count-1, stride]
+    }
+
     subscript (oEN: OpenEndN) -> Slice<Element> {
         return self[oEN.start...self.count-2]
     }
+
+    subscript (oEN: OpenEndN, stride: Int) -> Slice<Element> {
+        return self[oEN.start...self.count-2, stride]
+    }
+
     subscript (oS: OpenStart) -> Slice<Element> {
         return self[0...oS.end]
     }
+
+    subscript (oS: OpenStart, stride: Int) -> Slice<Element> {
+        return self[0...oS.end, stride]
+    }
+
     subscript(cR: ClosedRange) -> Slice<Element> {
-        let start = cR.start >= 0 ? cR.start : self.count + cR.start
-        let end   = cR.end   >= 0 ? cR.end   : self.count + cR.end - 1
+        return self[cR, 1]
+    }
+    subscript(cR: ClosedRange, stride: Int) ->Slice<Element> {
+        var start = cR.start >= 0 ? cR.start : self.count + cR.start
+        var end   = cR.end   >= 0 ? cR.end   : self.count + cR.end - 1
 
         var r: Slice<Element> = []
-        for var i = start ; i <= end ; i+=1{
-            r.append(self[i])
+        if stride < 0{
+            for var i = end; i >= start; i+=stride{
+                r.append(self[i])
+            }
+        } else {
+            for var i = start ; i <= end ; i+=stride{
+                r.append(self[i])
+            }
         }
         return r
     }
 }
 
-let x = [0,1,2,3,4,5,6,7,8,9]
+let x = [0,1,2,3,4,5,6,7,8,9,
+         10,11,12,13,14,15]
 
-let a = x[4...]
-let b = x[(-3)...]
+    // Open end
+var foo = x[3...]
+    foo = x[(-3)...]
 
-let c = x[4..<]
-let d = x[(-3)..<]
+    // Open end with step
+    foo = x[3..., 4]
+    foo = x[(-3)..., 3]
 
-let e = x[...6]
-let f = x[...(-4)]
+    // Open end, exclusive
+    foo = x[3..<]
+    foo = x[(-3)..<]
 
-let g = x[..<6]
-let h = x[..<(-4)]
+    // Open end, exclusive, with step
+    foo = x[3..<, 4]
+    foo = x[(-3)..<, 3]
 
-let i = x[2...5]
-let j = x[3...(-1)]
-let k = x[(-5)...8]
-let l = x[(-5)...(-2)]
+    // Open start
+    foo = x[...6]
+    foo = x[...(-4)]
 
-let m = x[2..<5]
-let n = x[3..<(-1)]
-let o = x[(-5)..<8]
-let p = x[(-5)..<(-2)]
+    // Open start, with step
+    foo = x[...6, 5]
+    foo = x[...(-4), 2]
+
+    // Open start, exclusive
+    foo = x[..<6]
+    foo = x[..<(-4)]
+
+    // Open start, exclusive, with step
+    foo = x[..<6, 5]
+    foo = x[..<(-4), 2]
+
+    // Range
+    foo = x[2...5]
+    foo = x[3...(-1)]
+    foo = x[(-7)...13]
+    foo = x[(-5)...(-2)]
+
+    // Range, with step
+    foo = x[2...5, 2]
+    foo = x[3...(-1), 3]
+    foo = x[(-7)...13, 2]
+    foo = x[(-5)...(-2), 2]
+
+    // Range, exclusive
+    foo = x[2..<5]
+    foo = x[3..<(-1)]
+    foo = x[(-7)..<13]
+    foo = x[(-5)..<(-2)]
+
+    // Range, exclusive, with step
+    foo = x[2..<5, 2]
+    foo = x[3..<(-1), 3]
+    foo = x[(-7)..<13, 2]
+    foo = x[(-5)..<(-2), 2]
